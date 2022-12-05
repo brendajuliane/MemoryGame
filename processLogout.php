efbc829a32accd6ca75175600dbfe30e392691f9
Brenda Juliane <bre8juliane@hotmail.com>
2022-12-05 18:56:27 -0300

    erro de logout corrigido

--- processLogout.php
+++ processLogout.php
@@ -1,9 +1,7 @@
 <?php 
-
+    session_start();
     if(isset($_SESSION['user'])){
         session_destroy();
-        header("Location:login.html");
-    } else {
         header("Location:login.html");
     }
 ?>
